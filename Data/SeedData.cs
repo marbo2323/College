7c31eb4aad9db9c4c896f184745c5602dd6c94b3
Arbo Lindma <arbolindma@gmail.com>
2026-02-13 19:55:18 +0200

    change seeding data

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -1,4 +1,5 @@
 using College.Data;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -16,23 +17,40 @@ public static class SeedData
         {
             // Seeding Students. 
             // Look for any students.
-            if (context.Student.Any())
+            if (!context.Student.Any())
             {
-                return;   // DB has been seeded
+                // DB has not been seeded with student data
+                context.Student.AddRange(
+                    new Student { FirstName = "George", LastName = "Teemus", EnrollmentDate = DateTime.Now, },
+                    new Student { FirstName = "Among", LastName = "us", EnrollmentDate = DateTime.Now, },
+                    new Student { FirstName = "Must", LastName = "Washhands", EnrollmentDate = DateTime.Now, },
+                    new Student { FirstName = "Mao", LastName = "Zedong", EnrollmentDate = DateTime.Now, },
+                    new Student { FirstName = "Benito", LastName = "Mussolini", EnrollmentDate = DateTime.Now, },
+                    new Student { FirstName = "Antony", LastName = "Martial", EnrollmentDate = DateTime.Now, },
+                    new Student { FirstName = "Cristiano", LastName = "Ronaldo", EnrollmentDate = DateTime.Now, },
+                    new Student { FirstName = "Donald", LastName = "Trump", EnrollmentDate = DateTime.Now, }
+                );
+                context.SaveChanges();
             }
 
-            context.Student.AddRange(
-                new Student {FirstName = "George",LastName = "Teemus",EnrollmentDate = DateTime.Now, },
-                new Student {FirstName = "Among",LastName = "us",EnrollmentDate = DateTime.Now, },
-                new Student {FirstName = "Must",LastName = "Washhands",EnrollmentDate = DateTime.Now, },
-                new Student {FirstName = "Mao",LastName = "Zedong",EnrollmentDate = DateTime.Now, },
-                new Student {FirstName = "Benito",LastName = "Mussolini",EnrollmentDate = DateTime.Now, },
-                new Student {FirstName = "Antony",LastName = "Martial",EnrollmentDate = DateTime.Now, },
-                new Student {FirstName = "Cristiano",LastName = "Ronaldo",EnrollmentDate = DateTime.Now, },
-                new Student {FirstName = "Donald",LastName = "Trump",EnrollmentDate = DateTime.Now, }
-            );
-            context.SaveChanges();
-             
+            // Seeding Courses. 
+            // Look for any courses.            
+            if (!context.Course.Any())
+            {
+                // DB has not been seeded with courses data
+                context.Course.AddRange(
+                    new Course { Title = "Programmeerimise alused", Credits = 3 },
+                    new Course { Title = "Programmeerimine 1", Credits = 6 },
+                    new Course { Title = "Programmeerimine 2", Credits = 6 },
+                    new Course { Title = "Tarkvara Arendusprotsess", Credits = 3 },
+                    new Course { Title = "Multimeedia", Credits = 3 },
+                    new Course { Title = "Hajusrakenduste Arendus", Credits = 3 }
+                );
+                context.SaveChanges();
+            }
+            var programmingBasics = context.Course.Where(course => course.Title == "Programmeerimise alused").First();
+            //Console.WriteLine("programmingBasics ID: " + programmingBasics.CourseId);
+
         }
 
     }
